afedba49c835702f0afdb8039de854f065b1c492
Mauro <mmcanhao@gmail.com>
2020-09-24 09:14:37 +0100

    Initial Commit

---
+++ GridSearchFromJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridSearchFromJSON
+//
+//  Created by Mauro Canhao on 24/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridSearchFromJSON/GridSearchFromJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchFromJSONApp.swift
+//  GridSearchFromJSON
+//
+//  Created by Mauro Canhao on 24/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchFromJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
